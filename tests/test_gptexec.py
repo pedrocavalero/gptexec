153cc53d79453defd2151ba3f5a786880ca02ba9
Pedro Cavaléro <590679+pedrocavalero@users.noreply.github.com>
2023-11-16 01:24:05 +0000

    Added test for action_getFile function

--- tests/test_gptexec.py
+++ tests/test_gptexec.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from gptexec import action_exec, action_updateFile
+from gptexec import action_exec, action_updateFile, action_getFile
 
 def test_action_exec():
     result = action_exec('echo "Hello, World!"')
@@ -18,3 +18,16 @@ def test_action_updateFile():
     os.remove(test_path)
     # Assert test content is written correctly
     assert content == test_content
+
+def test_action_getFile():
+    test_path = 'testfile.txt'
+    test_content = 'Test file content'
+    # Write test content to a file
+    with open(test_path, 'w') as file:
+        file.write(test_content)
+    # Use action to retrieve the content
+    result = action_getFile(test_path)
+    # Clean up test file
+    os.remove(test_path)
+    # Assert the content is retrieved correctly
+    assert result['content'] == test_content
